cd65e11fb851bfdabf7ecc4076e9372e2d03c476
Lukew23 <lwol078@aucklanduni.ac.nz>
2015-09-25 19:06:58 +1200

    created scene with combat manager game object and combatmanager script file

---
+++ Pirate Bay/Assets/Scripts/CombatManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CombatManager : MonoBehaviour {
+    public Text combatText;
+    private enum State {CombatStart}
+    private State state;
+	// Use this for initialization
+	void Start () {
+        state = State.CombatStart;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        combatText.text = StateToString(state);
+	}
+
+    string StateToString(State s){
+        switch(s)
+        {
+            case State.CombatStart: return "Combat Start";
+            default: return "Unknown State";
+        }
+    }
+}
