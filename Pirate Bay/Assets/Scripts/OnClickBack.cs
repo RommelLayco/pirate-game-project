dc779b2f771891c428d161db938339111483ba73
Anmol Desai <anmoldesai@Anmols-MacBook-Pro.local>
2015-09-27 15:24:56 +1300

    changing between map and ship done

---
+++ Pirate Bay/Assets/Scripts/OnClickBack.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnClickBack : MonoBehaviour {
+
+	void Update() {
+		if (Input.GetMouseButton (0))
+			clicked ();
+	}
+	
+	void clicked(){
+		Application.LoadLevel ("Ship");
+	}
+}
---
+++ Pirate Bay/Assets/Scripts/OnMouseClick.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnMouseClick : MonoBehaviour {
+
+	void Update() {
+		if (Input.GetMouseButton (0))
+			clicked ();
+	}
+
+	void clicked(){
+		Application.LoadLevel ("Map");
+	}
+}
