f4c21d19a92cb30d3c2a355647d5ac4a00c7290e
Daniel <DBrocx@gmail.com>
2015-09-29 10:48:16 +1300

    Have ship moving between islands. Movement logic and proximity detection needs slight work, but its working, and seemingly stable

--- Pirate Bay/Assets/Scripts/IslandController.cs
+++ Pirate Bay/Assets/Scripts/IslandController.cs
@@ -2,8 +2,15 @@
 using System.Collections;
 
 public class IslandController : MonoBehaviour {
+    public GameObject ship;
+
+    void Start() {
+        ship = GameObject.Find("topDownShip");
+        Debug.Log(ship.transform.position);
+    }
+
     void Update() {
-        foreach (Touch t in Input.touches) {
+       /* foreach (Touch t in Input.touches) {
             if (t.phase == TouchPhase.Ended) {
                 bool contained = gameObject.GetComponent<Collider>().bounds.Contains(t.position);
                 if (contained) {
@@ -11,9 +18,14 @@ public class IslandController : MonoBehaviour {
                 }
             }
         }
+        */
     }
-    void OnMouseDown() {
-        Debug.Log("On MouseDown");
+    void OnMouseUp() {
+        Debug.Log("On MouseUP");
+        Vector2 target = ship.GetComponent<topDownShipController>().targetLocation;
+        target.x = gameObject.transform.position.x;
+        target.y = gameObject.transform.position.y;
+        ship.GetComponent<topDownShipController>().targetLocation = target;
     }
 
 }
---
+++ Pirate Bay/Assets/Scripts/topDownShipController.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class topDownShipController : MonoBehaviour {
+    public Vector3 targetLocation;
+    private int speed;
+
+    // Use this for initialization
+    void Start() {
+        speed = 1;
+        targetLocation = transform.position;
+    }
+
+    void Update() {
+        bool reachedTarget = atTarget();
+        if (!reachedTarget) {
+            //Move and RNG of encountering a ship battle
+            moveToTarget();
+
+        } else {
+            //must be at target
+           // Debug.Log("reached Target");
+        }
+        Debug.Log(targetLocation);
+    }
+    void moveToTarget() {
+        Debug.Log("Moving from " + gameObject.transform.position + " to " + targetLocation);
+        //break this down into components.
+        Vector3 move = new Vector3(0, 0, 0);
+        if (transform.position.x > targetLocation.x) {
+            //needs to move left
+            move. x = -1;
+        } else {
+            //needs to move right
+            move.x = 1;
+        }
+
+        if (transform.position.y > targetLocation.y) {
+            //needs to move down
+            move.y = -1;
+        } else {
+            //needs to move up
+            move.y = 1;
+        }
+        transform.position += move * speed * (3 * Time.deltaTime)/2;
+
+    }
+
+    bool atTarget() {
+        float dist = Vector3.Distance(transform.position, targetLocation);
+        Vector3 distance = transform.position - targetLocation;
+        float actualDistance = distance.sqrMagnitude;
+        if (actualDistance <= 6) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    void startShipBattle() {
+        Debug.Log("Starting the ship battle sequence");
+    }
+
+}
