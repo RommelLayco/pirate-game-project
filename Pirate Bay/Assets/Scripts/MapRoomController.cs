8c69dc15262c9dbd6f85dd0e6214b77a593d817c
Anmol Desai <anmoldesai@wifi-172-23-83-112.uoa-wifi.auckland.ac.nz>
2015-09-29 12:51:43 +1300

    added controller to prefab map room

--- Pirate Bay/Assets/Scripts/MapRoomController.cs
+++ Pirate Bay/Assets/Scripts/MapRoomController.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class MapRoomController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
@@ -10,6 +10,28 @@ public class NewBehaviourScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+
+		foreach (Touch t in Input.touches)
+		{
+			if (t.phase == TouchPhase.Ended)
+			{
+				
+				bool contained = gameObject.GetComponent<Collider>().bounds.Contains(t.position);
+				if(contained)
+					clicked();
+			}
+			
+		}
 	
+	}
+
+	void OnMouseDown(){
+		clicked ();
+	}
+
+
+	void clicked() {
+		Application.LoadLevel("ExtendableMap");
 	}
 }
--- Pirate Bay/Assets/Scripts/OnMouseClick.cs
+++ Pirate Bay/Assets/Scripts/OnMouseClick.cs
@@ -4,6 +4,21 @@ using System.Collections;
 public class OnMouseClick : MonoBehaviour {
 
     void Update() {
+
+		/**
+		foreach (Touch t in Input.touches)
+		{
+			if (t.phase == TouchPhase.Ended)
+			{
+
+				bool contained = gameObject.GetComponent<Collider>().bounds.Contains(t.position);
+				if(contained)
+					clicked();
+			}
+			
+		}
+
+
         if (Input.GetMouseButton(0)) {
             RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
 
@@ -11,9 +26,14 @@ public class OnMouseClick : MonoBehaviour {
                 clicked();
             }
         }
+
+**/
+        
     }
 
+	/**
     void clicked() {
         Application.LoadLevel("ExtendableMap");
     }
+    **/
 }
