0a0e7b49a55366cc2a3e7c0275bf51e65af48914
Rommel Layco <rj.layco@gmail.com>
2015-10-02 15:37:15 +1300

    added touch controls, when platform is mobile

--- Pirate Bay/Assets/Scripts/Maze/Player.cs
+++ Pirate Bay/Assets/Scripts/Maze/Player.cs
@@ -16,19 +16,44 @@ public class Player : MovingObject
     void Update()
     {
 
-       
-   
-       
-        if (Input.GetMouseButton(0))
+
+        //note the following code will be grey and not work 
+        //depending on the build settings platform
+        //for now it is set to pc hence the mobile will be grey and won't work
+        //changing to android will enable the touch control
+        //using pc for testing
+
+        //Check if we are running either in the Unity editor or in a standalone build.
+#if UNITY_STANDALONE || UNITY_WEBPLAYER
+
+        //only get position on mouse release
+        if (Input.GetMouseButtonUp(0))
         {
             Vector3 goal = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
-            
+
             Move(goal);
         }
 
+
+        //Check if we are running on iOS, Android, Windows Phone 8 or Unity iPhone
+#elif UNITY_IOS || UNITY_ANDROID || UNITY_WP8 || UNITY_IPHONE
+
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended) {
+            
+            // Get location of touch
+            Vector2 touchPosition = Input.GetTouch(0).position;
+
+            //turn into a vector3 to move
+            Vector3 goal = new Vector3(touchPosition.x, touchPosition.y, 0f);
+
+            Move(goal);
+        }
         
-       
+#endif
+
+
+
 
     }
 
