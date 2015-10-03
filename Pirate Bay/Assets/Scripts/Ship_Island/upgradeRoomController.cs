6189334db0f6d25eec72e940ed3db03971317d73
Daniel <DBrocx@gmail.com>
2015-10-03 15:59:07 +1300

    added information display to bunk room. Updates with button based off (currently) hardcoded values

--- Pirate Bay/Assets/Scripts/Ship_Island/upgradeRoomController.cs
+++ Pirate Bay/Assets/Scripts/Ship_Island/upgradeRoomController.cs
@@ -1,29 +1,43 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Linq;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class upgradeRoomController : MonoBehaviour {
     Text upgradeText;
+    public Text infoText;
     private int level;
     private int[] levels = { 1, 2, 3, 4, 5 };
     private int[] costs = { 100, 200, 300, 400, 500 };
+    private int[] capacities = { 2, 4, 6, 8, 10 };
 
     void Start() {
         upgradeText = gameObject.GetComponentInChildren<Text>();
         level = 1;
-        upgradeText.text = "Upgrade from level " + level + "? \n$" + costs[level -1] + "g";
+        setButtonText();
     }
+
+
 
     public void UpgradeRoom() {
         level = (level % levels.Length) + 1;
+        setButtonText();
+        setInfoText();
 
+    }
+
+    private void setInfoText() {
+        infoText.text = "Level: " + level + "\n Capacity: " + capacities[level - 1] + "/" + capacities.Max();
+    }
+
+    private void setButtonText() {
         /*
-        level ++;
-        if (level <= levels.Length) {
-            upgradeText.text = "Fully Upgraded";
-            GUI.enabled = false;
-        } else { */
-            upgradeText.text = "Upgrade from level " + level + "? \n$" + costs[level - 1] + "g";
+     level ++;
+     if (level <= levels.Length) {
+         upgradeText.text = "Fully Upgraded";
+         GUI.enabled = false;
+     } else { */
+        upgradeText.text = "Upgrade from level " + level + "? \n$" + costs[level - 1] + "g";
         //}
-
     }
 }
