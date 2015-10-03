0dcfa31bc27e8c51a1ee1d7fa54cda7bacd8b2fa
Daniel <DBrocx@gmail.com>
2015-10-03 20:50:33 +1300

    Fixed multiple Game managers, and moved GameManager Object to ship view, and is instantiated there

--- Pirate Bay/Assets/Scripts/Ship_Island/GameManager.cs
+++ Pirate Bay/Assets/Scripts/Ship_Island/GameManager.cs
@@ -2,24 +2,29 @@
 using System.Collections;
 
 public class GameManager : MonoBehaviour {
+    private static GameManager _instance;
 
 
-	public Vector3 targetLocation;
-	public Vector3 currentLocation;
+    public Vector3 targetLocation;
+    public Vector3 currentLocation;
 
 
-	void Awake(){
-		DontDestroyOnLoad (gameObject);
-	}
+    void Awake() {
+        //if we don't have an [_instance] set yet
+        if (!_instance) {
+            _instance = this;
+            //otherwise, if we do, kill this thing
+        } else {
+            Destroy(this.gameObject);
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
 
-	// Use this for initialization
-	void Start () {
-		targetLocation = new Vector3(-500,-500,-500);
-		currentLocation = new Vector3(-500,-500,-500);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // Use this for initialization
+    void Start() {
+        targetLocation = new Vector3(-500, -500, -500);
+        currentLocation = new Vector3(-500, -500, -500);
+    }
+
+
 }
--- Pirate Bay/Assets/Scripts/Ship_Island/MapRoomController.cs
+++ Pirate Bay/Assets/Scripts/Ship_Island/MapRoomController.cs
@@ -2,34 +2,23 @@
 using System.Collections;
 
 public class MapRoomController : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
-	}
 	
 	// Update is called once per frame
 	void Update () {
-
-
-		foreach (Touch t in Input.touches)
-		{
-			if (t.phase == TouchPhase.Ended)
-			{
+		foreach (Touch t in Input.touches){
+			if (t.phase == TouchPhase.Ended){
 				
 				bool contained = gameObject.GetComponent<Collider>().bounds.Contains(t.position);
-				if(contained)
-					clicked();
+                if (contained) {
+                    clicked();
+                }
 			}
-			
 		}
-	
 	}
 
 	void OnMouseDown(){
 		clicked ();
 	}
-
 
 	void clicked() {
 		Application.LoadLevel("ExtendableMap");
