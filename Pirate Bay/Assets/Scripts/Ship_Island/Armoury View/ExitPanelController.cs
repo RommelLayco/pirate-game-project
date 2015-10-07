7b30ca5b140cbdbf3cf82279c193e3be71c88e59
Anmol Desai <anmoldesai@Anmols-MacBook-Pro.local>
2015-10-08 01:25:59 +1300

    panel now exitable

---
+++ Pirate Bay/Assets/Scripts/Ship_Island/Armoury View/ExitPanelController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ExitPanelController : MonoBehaviour {
+
+	public void exitPanel(){
+
+		GameObject panel  = GameObject.FindGameObjectWithTag ("Panel");
+
+		Image[] images = panel.GetComponentsInChildren<Image> ();
+		foreach (Image r in images) {
+			r.enabled = false;
+		}
+
+		panel.GetComponent<Image> ().enabled = false;
+
+
+
+	}
+}
