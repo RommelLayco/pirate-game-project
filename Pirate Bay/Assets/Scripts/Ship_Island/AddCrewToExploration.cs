2a1fcf8da3eabb0beb1f8df532ab6c8c59f12af9
Daniel <DBrocx@gmail.com>
2015-10-08 15:25:49 +1300

    Have set up so you can select which crew are taken on the island exploration. Currently this seen opens instead of the armoury, but will be linked in once islands are fully implemented. No GUI set up for selected crew yet. Thats next to do

---
+++ Pirate Bay/Assets/Scripts/Ship_Island/AddCrewToExploration.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class AddCrewToExploration : MonoBehaviour {
+    private GameManager manager;
+    private Text text;
+
+    void Start() {
+
+        manager = GameManager.getInstance();
+        text = gameObject.GetComponentInChildren<Text>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (manager.explorers.Count == 3) {
+            gameObject.GetComponent<Button>().interactable = false;
+            text.text = "Dinghy to shore is full";
+
+        } else {
+            gameObject.GetComponent<Button>().interactable = true;
+            text.text = "Take this crew member";
+        }
+    }
+
+    public void onClick() {
+        //Need to add the current 
+        manager.explorers.Add(manager.crewMembers[manager.crewIndex]);
+        CrewScrollerForIslandSelection.scrollLeft();    
+
+    }
+
+}
