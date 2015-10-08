2136eca0a3a032ef3a46b25ef8b197f2a744e21b
Anmol Desai <anmoldesai@Anmols-MacBook-Pro.local>
2015-10-09 00:45:16 +1300

    armour can be chose

---
+++ Pirate Bay/Assets/Scripts/Ship_Island/Armoury View/ChooseArmour.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChooseArmour : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		foreach (Touch t in Input.touches)
+		{
+			if (t.phase == TouchPhase.Ended)
+			{
+				
+				bool contained = gameObject.GetComponent<Collider>().bounds.Contains(t.position);
+				if(contained)
+					clicked();
+			}
+			
+		}
+		
+	}
+	
+	void OnMouseDown(){
+		clicked ();
+	}
+	
+	
+	void clicked() {
+		GameObject currentShowingArmour = GameObject.FindGameObjectWithTag ("Armour");
+		currentShowingArmour.GetComponent<SpriteRenderer> ().sprite = gameObject.GetComponent<SpriteRenderer> ().sprite;
+	}
+}
