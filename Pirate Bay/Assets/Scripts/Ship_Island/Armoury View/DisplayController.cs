1725b6019e1f637f3882bb374265b919943b40f2
Anmol Desai <anmoldesai@wifi-172-23-91-168.uoa-wifi.auckland.ac.nz>
2015-10-12 11:43:44 +1300

    trying to add text

--- Pirate Bay/Assets/Scripts/Ship_Island/Armoury View/DisplayController.cs
+++ Pirate Bay/Assets/Scripts/Ship_Island/Armoury View/DisplayController.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 
 public class DisplayController : MonoBehaviour {
 
@@ -8,6 +9,9 @@ public class DisplayController : MonoBehaviour {
 	private List<Armour> armoury;
 
 	public GameObject empty;
+
+	public Text textPrefab;
+	
 
 	private int x;
 	private int y;
@@ -22,6 +26,8 @@ public class DisplayController : MonoBehaviour {
 
 		//Debug.Log ("" + armoury.Count);
 
+
+		// add the armour
 		for (int i=0; i < armoury.Count ; i++) {
 
 
@@ -32,10 +38,24 @@ public class DisplayController : MonoBehaviour {
 
 			temp.transform.SetParent(gameObject.transform,false);
 
+
+			Text t = (Text)Instantiate(textPrefab,new Vector3(x,y,0),Quaternion.identity);
+
+			t.transform.SetParent(gameObject.transform);
+
+			//GameObject t = new GameObject();
+			//t.AddComponent<TextMesh>();
+			//t.GetComponent<TextMesh>().text = "WATUP";
+			//t.GetComponent<TextMesh>().fontSize = 5;
+			//t.transform.position = new Vector3(temp.transform.position.x, temp.transform.position.y, 0);
+			//t.transform.SetParent(temp.transform,true);
+
+
 			x = x + 150;
 
 		}
 
+		// add the empty blocks
 		for (int i=0; i < armoury.Count ; i++) {
 			
 			
