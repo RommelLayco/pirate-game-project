ff29226863cd53361723bde5abd66d44c136aa34
Lukew23 <lwol078@aucklanduni.ac.nz>
2015-10-17 23:29:41 +1300

    removed annoying debugging line

--- Pirate Bay/Assets/Scripts/XPStuff.cs
+++ Pirate Bay/Assets/Scripts/XPStuff.cs
@@ -4,6 +4,5 @@ using System.Collections;
 public class XPStuff : MonoBehaviour {
 	void Update () {
         this.transform.SetSiblingIndex(1000);
-        Debug.Log(transform.GetSiblingIndex());
 	}
 }
