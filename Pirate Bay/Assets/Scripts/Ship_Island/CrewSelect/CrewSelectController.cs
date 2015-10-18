b8bb43ce554219c66bce50a1af2bbe8f96925ad1
Nicholas Molloy <nmol886@aucklanduni.ac.nz>
2015-10-18 23:42:09 +1300

    Icon now shows the rival that the island belongs to

--- Pirate Bay/Assets/Scripts/Ship_Island/CrewSelect/CrewSelectController.cs
+++ Pirate Bay/Assets/Scripts/Ship_Island/CrewSelect/CrewSelectController.cs
@@ -9,6 +9,7 @@ public class CrewSelectController : MonoBehaviour {
 	private GameObject panel;
 	public Sprite skullSprite;
 	public Sprite skullOutlineSprite;
+	public Image RivalHat;
 
     void Awake() {
         manager = GameManager.getInstance();
@@ -25,6 +26,26 @@ public class CrewSelectController : MonoBehaviour {
 			DisableButton();
         }
     }
+
+	void DrawHat() {
+		string rival = manager.GetIsland (manager.currentLocation).rival;
+		Color color = new Color (0, 0, 0);
+		if (rival == "white") {
+			color = new Color (1, 1, 1);
+		} else if(rival == "red") {
+			color = new Color (1, 0, 0);
+		} else if(rival == "blue") {
+			color = new Color (0, 0, 1);
+		}
+		          
+		RivalHat.color = color;
+		RivalHat.GetComponent<CanvasRenderer> ().SetAlpha (1);
+
+	}
+
+	void HideHat() {
+		RivalHat.GetComponent<CanvasRenderer> ().SetAlpha (0);
+	}
 
 	void ShowDifficulty() {
 		// Make Skull icons visible and set them to represent the level of the island
@@ -58,6 +79,7 @@ public class CrewSelectController : MonoBehaviour {
 		gameObject.GetComponent<CanvasRenderer> ().SetAlpha (0);
 		gameObject.GetComponentInChildren<Text>().color = Color.clear;
 		HideDifficulty ();
+		HideHat ();
 	}
 
 	void EnableButton() {
@@ -67,6 +89,7 @@ public class CrewSelectController : MonoBehaviour {
 		gameObject.GetComponent<CanvasRenderer> ().SetAlpha (1);
 		gameObject.GetComponentInChildren<Text>().color = textColor;
 		ShowDifficulty ();
+		DrawHat ();
 	}
 
     // Opens the menu scene that allows the user to select the crew members to take to the island
