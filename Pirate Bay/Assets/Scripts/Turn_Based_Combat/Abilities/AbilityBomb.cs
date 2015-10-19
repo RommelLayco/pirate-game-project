91dd782c345b62e876acef4297dc9127708032ee
zlu883 <zlu883@aucklanduni.ac.nz>
2015-10-20 11:33:15 +1300

    Increased power of bomb ability slightly

--- Pirate Bay/Assets/Scripts/Turn_Based_Combat/Abilities/AbilityBomb.cs
+++ Pirate Bay/Assets/Scripts/Turn_Based_Combat/Abilities/AbilityBomb.cs
@@ -16,7 +16,7 @@ public class AbilityBomb : Ability
         foreach (Combatant e in enemies)
         {
             if (!e.IsDead())
-                actions.Enqueue(new ActionAttack(me,e,.5f));
+                actions.Enqueue(new ActionAttack(me,e,.7f));
         }
         actions.Enqueue(new ActionPauseForFrames(30));
         actions.Enqueue(new ActionHideInfo());
